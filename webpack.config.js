1577d857c2b3e70925ccaf93ef225332782efa81
유지영 <yj0zero@gmail.com>
2022-12-06 14:30:31 +0900

    Create project w.webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".vue", ".js"], //생략할 확장자 작성
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    }, //별칭 사용, 절대경로로 사용된다.
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    filename: "hello.js", //생략하면 entry의 이름으로 저장됨
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/, //.vue 확장자를 찾는 정규표현식
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "./src/index.html", //path.resolve를 작성해야하지만 내장되어 있으므로 생략해도 된다.
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "static/favicon.ico" }],
+    }),
+  ], //플러그인을 가져와야 제대로 동작된다.
+};
